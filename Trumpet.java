851b1685218b43dcca9fc6fa5fd95a9c0da66be4
=vajreshwarixworkz <=vajreshwarikollixworkz@gmail.com>
2024-08-01 11:58:18 +0530

    files

---
+++ Trumpet.java
@@ -0,0 +1,12 @@
+class Trumpet{
+	String type="piccolo";
+	String brand;
+	String color;
+	double price;
+	Trumpet(String brandLocal,String colorLocal)
+	{
+		brand=brandLocal;
+		color=colorLocal;
+		System.out.println("created Trumpet using string constructor");
+	}
+}
